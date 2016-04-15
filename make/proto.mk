cc24ede3ab64f8b69b494718ed720e98efef5610
tp <tp@octavore.com>
2016-04-14 23:43:37 -0700

    protos: Tweak makefile.

--- make/proto.mk
+++ make/proto.mk
@@ -4,9 +4,11 @@ PROTO_FILES=$(shell find . -name "*.proto")
 # includes for all subfolders containing proto files, e.g. `-I./path/to/proto/folder`
 INCL_PROTO_DIR=$(shell find . -name "*.proto" -exec dirname {} \; | sort -u | sed -e 's/^/-I/')
 
+PROTO_PREFIX=import_prefix_proto=github.com/octavore/press/proto/
+
 protos:
 	@mkdir -p proto
-	protoc $(INCL_PROTO_DIR) $(PROTO_FILES) --go_out=plugins=setter+grpc:./proto
+	protoc $(INCL_PROTO_DIR) $(PROTO_FILES) --go_out=$(PROTO_PREFIX):./proto
 
 prepare:
 	brew install grpc/grpc/google-protobuf
