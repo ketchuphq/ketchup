84aa3e770f530f1fcb3deaec1ac4af9eb8ba13ad
tp <tp@octavore.com>
2016-11-30 21:45:55 -0800

    server: Modularize renderer, proto fixes.

--- server/content/render_content.go
+++ server/content/render_content.go
@@ -1,29 +1,27 @@
 package content
 
 import (
-	"errors"
-
-	"html/template"
+	"fmt"
 
 	"github.com/octavore/press/proto/press/models"
 )
 
 type contentMap map[string]interface{}
 
-var contentRenders = map[string]func(string) (interface{}, error){
-	models.ContentType_html.String(): renderHTMLContent,
-	// models.ContentType_markdown.String(): renderHTMLContent,
+var renderers = map[string]Content{
+	models.Content_html.String(): &HTMLContent{},
 }
 
 func renderPageContents(page *models.Page) (contentMap, error) {
 	contents := map[string]interface{}{}
 	var err error
 	for _, c := range page.Contents {
-		renderer := contentRenders[c.GetContentType().String()]
+		rt := c.GetContentType().String()
+		renderer := renderers[rt]
 		if renderer == nil {
-			return nil, errors.New("unknown renderer")
+			return nil, fmt.Errorf("contents: unknown renderer %q", rt)
 		}
-		contents[c.GetKey()], err = renderer(c.GetValue())
+		contents[c.GetKey()], err = renderer.Render(c.GetValue())
 		if err != nil {
 			return nil, err
 		}
@@ -31,6 +29,3 @@ func renderPageContents(page *models.Page) (contentMap, error) {
 	return contents, err
 }
 
-func renderHTMLContent(s string) (interface{}, error) {
-	return template.HTML(s), nil
-}
