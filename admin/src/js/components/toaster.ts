406ec05812f5ad6b07d313572fd1a33ac53539d6
tp <tp@octavore.com>
2016-11-30 21:53:36 -0800

    admin: Toaster component.

---
+++ admin/src/js/components/toaster.ts
@@ -0,0 +1,65 @@
+const sweepInterval = 500;
+let toasts: Toast[] = [];
+
+class Toast {
+  public readonly key: number;
+  public entered: boolean;
+  private readonly expires: number;
+  constructor(public message: string, expiresSeconds: number, public klass: string = '') {
+    let now = new Date().getTime();
+    this.expires = now + expiresSeconds;
+    this.key = now;
+    this.entered = false;
+  }
+
+  expired(now = new Date().getTime()) {
+    return this.expires < now;
+  }
+
+  getClass() {
+    if (this.klass) {
+      return `toast--${this.klass}`;
+    }
+    return 'toast--green';
+  }
+}
+
+setInterval(() => {
+  let now = new Date().getTime() - 2000;
+  let prev = toasts.length;
+  let hasChange = toasts.reduce((prev, cur) => {
+    return prev || cur.expired() != cur.expired(now);
+  }, false);
+  toasts = toasts.filter((t) => !t.expired(now));
+  if (prev != toasts.length || hasChange) {
+    m.redraw();
+  }
+}, sweepInterval);
+
+export function add(message: string, klass: string = '') {
+  toasts.push(new Toast(message, 3000, klass));
+  m.redraw();
+}
+
+export function render() {
+  return m('.toast-wrapper',
+    {
+      key: 'toaster',
+      className: toasts.length == 0 ? 'toast-wrapper--hidden' : '',
+      config: (el: HTMLElement, isInitialized: boolean) => {
+        // prevents animation on route change
+        if (!isInitialized) {
+          for (var i = 0; i < el.children.length; i++) {
+            var element = el.children[i];
+            element.classList.add('toast--noanimate');
+          }
+        }
+      }
+    },
+    toasts.map((t) =>
+      m('.toast.toast--enter', {
+        key: t.key,
+        className: [t.getClass(), t.expired() ? 'toast--expired' : ''].join(' '),
+      }, m('.contents', t.message))
+    ));
+}
