391516004bd083a55732b58e814cbcb8ac3fcd50
tp <tp@octavore.com>
2016-12-07 06:57:18 -0800

    admin: Fancy collapsing sidebar.

--- admin/src/js/components/navigation.ts
+++ admin/src/js/components/navigation.ts
@@ -1,16 +1,28 @@
 import { AuthController } from 'components/auth';
 
+let collapsed = false;
+
 export default class NavigationComponent extends AuthController {
+  collapsed: Mithril.Property<boolean>;
   constructor() {
     super();
+    this.collapsed = m.prop(collapsed);
+  }
+
+  toggle() {
+    collapsed = !this.collapsed();
+    this.collapsed(collapsed);
   }
 
   static controller = NavigationComponent;
-  link(url: string, text: string, additionalClasses: string = '') {
-    return m(`a.nav-link${additionalClasses}`, {
+  link(url: string, text: string, opts: { additionalClasses?: string, icon?: string } = {}) {
+    return m(`a.nav-link${opts.additionalClasses || ''}`, {
       href: url,
       config: m.route
-    }, text);
+    }, [
+        opts.icon != '' ? m(`span.typcn.typcn-${opts.icon}`) : '',
+        m('span.nav-link__text', text)
+      ]);
   }
 
   static view(ctrl: NavigationComponent) {
@@ -23,20 +35,30 @@ export default class NavigationComponent extends AuthController {
         ctrl.link('/admin/login', 'Login')
       ]);
     }
-    return m('.container--navigation', [
-      ctrl.link('/admin', 'ketchup', '.nav-title'),
-      m('.nav-link',
-        m('a.button.button--green.button--center', {
-          href: '/admin/compose',
-          config: m.route
-        }, 'Compose')
-      ),
-      ctrl.link('/admin/routes', 'Routes'),
-      ctrl.link('/admin/pages', 'Pages'),
-      ctrl.link('/admin/themes', 'Theme'),
-      ctrl.link('/admin/settings', 'Settings'),
-      ctrl.link('/admin/logout', 'Logout')
-    ]);
+    return m('.container--navigation', {
+      className: ctrl.collapsed() ? 'container--navigation--hidden' : '',
+    }, [
+        ctrl.link('/admin', 'ketchup', { additionalClasses: '.nav-title' }),
+        m('.nav-button',
+          m('a.button.button--green.button--center', {
+            href: '/admin/compose',
+            config: m.route
+          },
+            m('span.typcn.typcn-edit'),
+            m('span.nav-link__text', 'Compose')
+          )
+        ),
+        ctrl.link('/admin/routes', 'Routes', { icon: 'flow-children' }),
+        ctrl.link('/admin/pages', 'Pages', { icon: 'document-text' }),
+        ctrl.link('/admin/themes', 'Theme', { icon: 'brush' }),
+        ctrl.link('/admin/settings', 'Settings', { icon: 'spanner-outline' }),
+        ctrl.link('/admin/logout', 'Logout', { icon: 'weather-night' }),
+        m(`a.nav-link.nav-link--toggle`, {
+          onclick: () => { ctrl.toggle(); }
+        },
+          m(`span.typcn.typcn-arrow-${ctrl.collapsed() ? 'maximise' : 'minimise'}`),
+        )
+      ]);
   };
 }
 
