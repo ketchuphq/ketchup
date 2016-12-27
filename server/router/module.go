094ae253fbc945a714c5feed26d9be990c926081
tp <tp@octavore.com>
2016-12-27 09:27:24 -0800

    server: Better error logging in router.

--- server/router/module.go
+++ server/router/module.go
@@ -1,23 +1,25 @@
 package router
 
 import (
-	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/go-errors/errors"
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 	"github.com/julienschmidt/httprouter"
 	"github.com/octavore/naga/service"
+	"github.com/octavore/nagax/logger"
 	"github.com/octavore/nagax/router"
-
 	"github.com/octavore/press/proto/press/api"
 )
 
-var ErrNotFound = errors.New("not found")
+var ErrNotFound = fmt.Errorf("not found")
 
 type Module struct {
 	*router.Module
+	Logger *logger.Module
 }
 
 func (m *Module) Init(c *service.Config) {
@@ -31,22 +33,33 @@ func (m *Module) Subrouter(path string) *httprouter.Router {
 
 func Proto(rw http.ResponseWriter, pb proto.Message) error {
 	rw.Header().Set("Content-Type", "application/json")
-	return JSON.Marshal(rw, pb)
+	err := JSON.Marshal(rw, pb)
+	if err != nil {
+		return fmt.Errorf("router: %v", err)
+	}
+	return nil
 }
 
-func InternalError(rw http.ResponseWriter, err error) {
+func (m *Module) InternalError(rw http.ResponseWriter, err error) {
 	if err == ErrNotFound {
 		NotFound(rw)
 		return
 	}
-	log.Println(err)
+
+	switch e := err.(type) {
+	case *errors.Error:
+		s := e.StackFrames()[0]
+		m.Logger.Errorf("[%s:%d] %v", s.Package, s.LineNumber, e.Error())
+	default:
+		m.Logger.Error(e)
+	}
 	rw.WriteHeader(http.StatusInternalServerError)
 	err = Proto(rw, &api.Error{
 		Code:   api.ErrorCode_INTERNAL_SERVER_ERROR.Enum(),
 		Detail: proto.String("Internal server error."),
 	})
 	if err != nil {
-		log.Println(err)
+		log.Println("router: ", err)
 	}
 }
 
@@ -57,7 +70,7 @@ func NotFound(rw http.ResponseWriter) {
 		Detail: proto.String("Not found."),
 	})
 	if err != nil {
-		log.Println(err)
+		log.Println("router: ", err)
 	}
 }
 
