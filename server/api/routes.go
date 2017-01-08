e52e8de214118b7201dc5f8b6b9d7b491466d703
tp <tp@octavore.com>
2017-01-08 14:23:45 -0800

    server: Server side route cleaning.

--- server/api/routes.go
+++ server/api/routes.go
@@ -1,16 +1,41 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+	"regexp"
+	"strings"
 
-	"github.com/golang/protobuf/jsonpb"
 	"github.com/julienschmidt/httprouter"
+
+	"io/ioutil"
 
 	"github.com/octavore/press/proto/press/api"
 	"github.com/octavore/press/proto/press/models"
 	"github.com/octavore/press/server/router"
 	"github.com/octavore/press/util/errors"
 )
+
+var (
+	re1 = regexp.MustCompile(`[^a-zA-Z0-9\/]`)
+	re2 = regexp.MustCompile(`^-+`)
+	re3 = regexp.MustCompile(`-+$`)
+	re4 = regexp.MustCompile(`\/\/+`)
+)
+
+func formatRoute(r *models.Route) *models.Route {
+	if r.Path == nil {
+		return r
+	}
+	p := "/" + strings.Trim(r.GetPath(), "/")
+	p = strings.ToLower(p)
+	p = re1.ReplaceAllString(p, "-")
+	p = re2.ReplaceAllString(p, "")
+	p = re3.ReplaceAllString(p, "")
+	p = re4.ReplaceAllString(p, "/")
+	r.Path = &p
+	return r
+}
 
 func (m *Module) ListRoutes(rw http.ResponseWriter, req *http.Request, par httprouter.Params) error {
 	routes, err := m.DB.ListRoutes()
@@ -41,11 +66,15 @@ func (m *Module) ListRoutesByPage(rw http.ResponseWriter, req *http.Request, par
 
 func (m *Module) UpdateRoute(rw http.ResponseWriter, req *http.Request, par httprouter.Params) error {
 	route := &models.Route{}
-	err := jsonpb.Unmarshal(req.Body, route)
+	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		return errors.Wrap(err)
 	}
-	err = m.DB.UpdateRoute(route)
+	err = json.Unmarshal(b, route)
+	if err != nil {
+		return errors.Wrap(err)
+	}
+	err = m.DB.UpdateRoute(formatRoute(route))
 	if err != nil {
 		return err
 	}
@@ -68,7 +97,11 @@ func (m *Module) DeleteRoute(rw http.ResponseWriter, req *http.Request, par http
 func (m *Module) UpdateRoutesByPage(rw http.ResponseWriter, req *http.Request, par httprouter.Params) error {
 	pageUUID := par.ByName("uuid")
 	pb := &api.UpdatePageRoutesRequest{}
-	err := jsonpb.Unmarshal(req.Body, pb)
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return errors.Wrap(err)
+	}
+	err = json.Unmarshal(b, pb)
 	if err != nil {
 		return errors.Wrap(err)
 	}
@@ -97,7 +130,7 @@ func (m *Module) UpdateRoutesByPage(rw http.ResponseWriter, req *http.Request, p
 	}
 
 	for _, route := range newList {
-		err = m.DB.UpdateRoute(route)
+		err = m.DB.UpdateRoute(formatRoute(route))
 		if err != nil {
 			return err
 		}
