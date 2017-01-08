4458d8406886df1614991dfc93706800d3b33652
tp <tp@octavore.com>
2017-01-08 14:33:21 -0800

    admin: Improve route formatting logic.

--- admin/src/js/components/edit_route.ts
+++ admin/src/js/components/edit_route.ts
@@ -3,12 +3,8 @@ import Page from 'lib/page';
 
 export default class EditRoutesComponent {
   dirty: boolean;
-  infer: () => string;
 
   constructor(public page: Page) {
-    this.infer = () => {
-      return this.dirty ? '' : page.name;
-    };
     this.dirty = true;
     if (page.routes.length == 0) {
       page.routes.push(new Route());
@@ -18,14 +14,31 @@ export default class EditRoutesComponent {
     }
   }
 
+  infer() {
+    if (this.dirty) {
+      return;
+    }
+    if (this.page.routes.length < 1) {
+      return;
+    }
+    if (!!this.page.routes[0].path) {
+      return;
+    }
+    if (!this.page.name || this.page.name.trim() == '') {
+      return;
+    }
+    this.page.routes[0].path = Route.format(this.page.name);
+  };
+
   routeEditor(route: Route, i: number) {
+    this.infer();
     return m('div', [
       m('input[type=text]', {
         placeholder: '/path/to/page',
-        value: Route.format(this.infer() || route.path || ''),
+        value: Route.format(route.path),
         onchange: m.withAttr('value', (v) => {
           this.dirty = true;
-          route.path = v;
+          route.path = Route.format(v);
         })
       }),
       i == 0 ? '' :
