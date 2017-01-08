ae160d24aa2f1265ca39fd63e5fa3ba982a0179b
tp <tp@octavore.com>
2017-01-08 14:28:22 -0800

    admin: Allow dummy store for browsers which don't support it (safari private mode).

--- admin/src/js/components/auth.ts
+++ admin/src/js/components/auth.ts
@@ -10,14 +10,38 @@ interface Preferences {
   hideMenu: boolean;
 }
 
+interface Storer {
+  get(key: string): any;
+  set(key: string, val: any): void;
+}
+
+class DummyStore {
+  data: any;
+
+  constructor() {
+    this.data = {};
+  }
+
+  get(key: string): any {
+    return this.data[key];
+  }
+
+  set(key: string, val: any) {
+    this.data[key] = val;
+  }
+}
+
 let cachedUser: User = null;
 
 // AuthController is a super class for controllers which may require auth
 export class AuthController {
   user: Mithril.BasicProperty<User>;
   _userPromise: Mithril.Promise<User>;
+  store: Storer;
+
 
   constructor(user: User = null) {
+    this.store = store.disabled ? new DummyStore() : store;
     this.user = m.prop<User>(user || cachedUser);
     if (this.user()) {
       var deferred = m.deferred<User>();
@@ -66,12 +90,12 @@ export class AuthController {
     if (!this.user()) {
       return null;
     }
-    let prefs: Preferences = store.get(this.storeKey);
+    let prefs: Preferences = this.store.get(this.storeKey);
     if (!prefs) {
       prefs = {
         hideMenu: false,
       };
-      store.set(this.storeKey, prefs);
+      this.store.set(this.storeKey, prefs);
     }
     return prefs;
   }
@@ -93,7 +117,7 @@ export class AuthController {
     }
     let prefs: Preferences = this.getPrefs();
     prefs[key] = val;
-    store.set(this.storeKey, prefs);
+    this.store.set(this.storeKey, prefs);
   }
 }
 
