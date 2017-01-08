d34187d46f434c571c18a5834aaf78bd83d3b881
tp <tp@octavore.com>
2017-01-08 14:36:55 -0800

    admin: Add zen mode to quill editor.

--- admin/src/js/components/quill.ts
+++ admin/src/js/components/quill.ts
@@ -6,10 +6,14 @@ export default class QuillComponent {
   content: API.Content;
   element: HTMLElement;
   _id: string;
+  maximize: Mithril.BasicProperty<boolean>;
+  dark: Mithril.BasicProperty<boolean>;
 
   constructor(content: API.Content, readonly short: boolean = false) {
     this.content = content;
     this._id = 'quill' + Math.random().toString().slice(2, 10);
+    this.maximize = m.prop(false);
+    this.dark = m.prop(false);
   }
 
   get id(): string {
@@ -27,7 +31,10 @@ export default class QuillComponent {
   initializeQuill() {
     this.quill = new Quill(this.id, {
       placeholder: 'start typing...',
-      theme: 'snow'
+      theme: 'snow',
+      modules: {
+        toolbar: `${this.id}-toolbar`
+      }
     });
     this.quill.on('text-change', () => {
       let editor = this.element.getElementsByClassName('ql-editor')[0];
@@ -37,7 +44,37 @@ export default class QuillComponent {
 
   static controller = QuillComponent;
   static view(ctrl: QuillComponent) {
-    return m(ctrl.klass,
+    return m(ctrl.klass, {
+      class: [
+        ctrl.maximize() ? 'ql-container-full' : '',
+        ctrl.dark() ? 'ql-container-full-dark' : ''
+      ].join(' ')
+    },
+      m(`${ctrl.id}-toolbar`, [
+        m('.ql-formats',
+          m('select.ql-size'),
+        ),
+        m('.ql-formats',
+          m('button.ql-bold'),
+          m('button.ql-italic'),
+          m('button.ql-underline'),
+          m('button.ql-link'),
+        ),
+        m('.ql-formats',
+          m('button.ql-list', { value: 'ordered' }),
+          m('button.ql-list', { value: 'bullet' }),
+        ),
+        m('.ql-formats',
+          m('button.ql-clean'),
+        ),
+        m('.ql-formats.qlx-maximize',
+          m('a', {
+            onclick: () => {
+              ctrl.maximize(!ctrl.maximize());
+            }
+          }, 'Zen mode')
+        )
+      ]),
       m(ctrl.id, {
         config: (el: HTMLElement, isInitialized: boolean) => {
           if (!isInitialized) {
@@ -48,7 +85,15 @@ export default class QuillComponent {
             ctrl.initializeQuill();
           }
         }
-      })
+      }),
+      ctrl.maximize() ? m('.qlx-controls', [
+        m('span.typcn.typcn-adjust-contrast', {
+          onclick: () => { ctrl.dark(!ctrl.dark()); }
+        }),
+        m('span.typcn.typcn-times', {
+          onclick: () => { ctrl.maximize(false); }
+        }),
+      ]) : ''
     );
   }
 }
