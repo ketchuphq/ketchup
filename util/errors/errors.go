4ba51077247ff90c33cb43d63afbce8f567c226e
tp <tp@octavore.com>
2017-01-14 22:23:00 -0800

    errors: Supports new wrapped error from string.

--- util/errors/errors.go
+++ util/errors/errors.go
@@ -1,15 +1,26 @@
 package errors
 
-import "github.com/go-errors/errors"
+import (
+	"fmt"
 
-func Wrap(e interface{}) error {
+	"github.com/go-errors/errors"
+)
+
+// New returns a new wrapped error with m as message
+func New(m string, a ...interface{}) error {
+	return errors.Wrap(fmt.Errorf(m, a...), 1)
+}
+
+// Wrap an error e if it is not nil
+func Wrap(e error) error {
 	if e == nil {
 		return nil
 	}
 	return errors.Wrap(e, 1)
 }
 
-func WrapS(e interface{}, skip int) error {
+// WrapS is like Wrap but skips 'skip' lines of trace
+func WrapS(e error, skip int) error {
 	if e == nil {
 		return nil
 	}
