489039d6b80509b7d7644a417957b9ccf5dda6d9
tp <tp@octavore.com>
2017-01-21 22:52:32 -0800

    admin: Tweak msx to support components.

--- admin/src/js/lib/msx.ts
+++ admin/src/js/lib/msx.ts
@@ -1,5 +1,8 @@
 import * as m from 'mithril';
 
-export default function msx(element: string, props: any, ...children: any[]) {
-  return m(element, props, children)
+export default function msx(element: string | Mithril.Component<any>, props: any, ...children: any[]) {
+  if (typeof(element) === 'string') {
+    return m(element, props, children);
+  }
+  return m.component(element, props, children);
 }
