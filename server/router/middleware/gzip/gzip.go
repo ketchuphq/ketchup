ebe86c705666a80b2e1e82c15bf09bc48f307ee9
tp <tp@octavore.com>
2017-02-05 18:50:30 -0800

    server: Tweak gzip middleware to only work on paths with extensions.

--- server/router/middleware/gzip/gzip.go
+++ server/router/middleware/gzip/gzip.go
@@ -1,20 +1,24 @@
 package gzip
 
 import (
+	"compress/gzip"
 	"net/http"
+	"path"
 
 	"github.com/NYTimes/gziphandler"
 )
 
-var Default = func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	handle := gziphandler.GzipHandler(next)
-	handle.ServeHTTP(rw, req)
-}
+var Default = New(gzip.DefaultCompression)
 
 func New(lvl int) func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
 	handler := gziphandler.MustNewGzipLevelHandler(lvl)
 
 	return func(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+		// todo: whitelist gzippable data
+		if path.Ext(req.URL.Path) == "" {
+			next(rw, req)
+			return
+		}
 		handle := handler(next)
 		handle.ServeHTTP(rw, req)
 	}
