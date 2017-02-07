23bf7bb82864f8d4d3d42c7d2d7d7c5cb837b55a
tp <tp@octavore.com>
2017-02-06 21:14:27 -0800

    server: Basic not found handler.

--- server/content/templates/module.go
+++ server/content/templates/module.go
@@ -69,6 +69,23 @@ func (m *Module) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	asset, err := m.GetAsset(req.URL.Path)
 	if err != nil {
 		m.Logger.Warning(err)
+		m.NotFound(rw, req)
+		return
+	}
+	if asset == nil {
+		m.NotFound(rw, req)
+		return
+	}
+	ext := path.Ext(req.URL.Path)
+	rw.Header().Add("Content-Type", mime.TypeByExtension(ext))
+	rw.Write([]byte(asset.GetData()))
+}
+
+// NotFound serves a 404 page from a 404.html asset, if it exists.
+func (m *Module) NotFound(rw http.ResponseWriter, req *http.Request) {
+	asset, err := m.GetAsset("/404.html")
+	if err != nil {
+		m.Logger.Warning(err)
 		http.NotFound(rw, req)
 		return
 	}
