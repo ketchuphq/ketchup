1d3428cc81ecee2fc01320113b9d3c99c180b9e4
tp <tp@octavore.com>
2017-03-13 00:46:11 -0700

    admin: Migate (unused) routes.tsx.

--- admin/src/js/pages/routes.tsx
+++ admin/src/js/pages/routes.tsx
@@ -1,36 +1,38 @@
 import msx from 'lib/msx';
+import * as m from 'mithril';
 import Route from 'lib/route';
-import Layout from 'components/layout';
 
 export default class RoutesPage {
-  routes: Mithril.BasicProperty<Route[]>;
+  routes: Route[];
 
   constructor() {
-    this.routes = m.prop([]);
-    Route.list().then((data) => this.routes(data));
+    this.routes = [];
+    Route.list().then((data) => this.routes = data);
   }
 
-  static controller = RoutesPage;
-  static view(ctrl: RoutesPage) {
-    return Layout(
-      <div class='routes'>
-        <h1>Routes</h1>
-        <div class='table'>
-          {ctrl.routes().map((r) =>
-            <div class='tr'>
-              <a href={r.path ? r.path : '#'}>{r.path}</a>
-              {
-                !r.pageUuid ? '' :
-                  <a class='list-link'
-                    href={`/admin/pages/${r.pageUuid}`}
-                    config={m.route}
-                  >
-                    edit page
-                  </a>
-              }
-            </div>)}
-        </div>
+  static oninit(v: Mithril.Vnode<{}, RoutesPage>) {
+    v.state = new RoutesPage();
+  }
+
+  static view(v: Mithril.Vnode<{}, RoutesPage>) {
+    let ctrl = v.state;
+    return <div class='routes'>
+      <h1>Routes</h1>
+      <div class='table'>
+        {ctrl.routes.map((r) =>
+          <div class='tr'>
+            <a href={r.path ? r.path : '#'}>{r.path}</a>
+            {
+              !r.pageUuid ? '' :
+                <a class='list-link'
+                  href={`/admin/pages/${r.pageUuid}`}
+                  config={m.route}
+                >
+                  edit page
+                </a>
+            }
+          </div>)}
       </div>
-    );
+    </div>;
   }
 }
