bc8e4fbba26e3647505f6fef1623d761b371e4c2
tp <tp@octavore.com>
2017-04-04 23:06:11 -0700

    proto: Use forked protoc-gen-go-2 and add struct.proto.

--- make/proto.mk
+++ make/proto.mk
@@ -1,14 +1,17 @@
+export PATH := $(GOPATH)/bin:$(PATH)
+
 # all proto files in subfolders
 PROTO_FILES=$(shell find . -name "*.proto")
 
 # includes for all subfolders containing proto files, e.g. `-I./path/to/proto/folder`
 INCL_PROTO_DIR=$(shell find . -name "*.proto" -exec dirname {} \; | sort -u | sed -e 's/^/-I/')
+INCL_WKT=-I $$GOPATH/src/github.com/golang/protobuf/ptypes/struct
 
 PROTO_PREFIX=import_prefix_proto=github.com/octavore/ketchup/proto/
 
 protos:
 	@mkdir -p proto
-	protoc $(INCL_PROTO_DIR) $(PROTO_FILES) --go_out=$(PROTO_PREFIX),plugins=setter:./proto
+	protoc $(INCL_PROTO_DIR) $(INCL_WKT) $(PROTO_FILES) $$GOPATH/src/github.com/golang/protobuf/ptypes/struct/struct.proto --go-2_out=$(PROTO_PREFIX),plugins=setter:./proto
 	go run util/gots/main.go ./admin/src/js/lib/api.ts
 
 protos_list:
@@ -16,4 +19,4 @@ protos_list:
 
 prepare:
 	brew install grpc/grpc/google-protobuf
-	go get -u github.com/golang/protobuf/protoc-gen-go
+	go get -u github.com/octavore/protobuf/protoc-gen-go-2
