fb99b955e6bb1af6630848a84287ab5c9ab7ac26
tp <tp@octavore.com>
2017-08-02 00:27:45 -0700

    admin: Improve msx types.

--- admin/src/js/lib/msx.ts
+++ admin/src/js/lib/msx.ts
@@ -1,14 +1,14 @@
 import * as m from 'mithril';
 
 export default function msx<A, S>(
-  element: string | m.Component<A, S>,
-  props: A & m.Lifecycle<A,S>,
+  element: string | m.ComponentTypes<A, S>,
+  props: A & m.Lifecycle<A, S>,
   ...children: any[]
-): m.Vnode<any, any> {
+): m.Vnode<A, S> {
   if (!element) {
     return null;
   }
-  if (typeof (element) === 'string') {
+  if (typeof element === 'string') {
     return m(element, props, children);
   }
   return m(element, props, children);
