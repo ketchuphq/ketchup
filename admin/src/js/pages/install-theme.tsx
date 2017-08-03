0a7ff812bbb52d5ca2b2b6a7b647f71f3a4fdf41
tp <tp@octavore.com>
2017-08-02 22:08:57 -0700

    admin: Show a modal on theme install.

--- admin/src/js/pages/install-theme.tsx
+++ admin/src/js/pages/install-theme.tsx
@@ -3,6 +3,10 @@ import * as m from 'mithril';
 import * as API from 'lib/api';
 import Theme from 'lib/theme';
 import { MustAuthController } from 'components/auth';
+import { LinkRow, Row } from 'components/table';
+import { ModalComponent } from 'components/modal';
+import { Loader } from 'components/loading';
+import * as Toaster from 'components/toaster';
 
 export default class InstallThemePage extends MustAuthController {
   installedThemes: { [key: string]: boolean };
@@ -12,74 +16,76 @@ export default class InstallThemePage extends MustAuthController {
   constructor() {
     super();
     this.installedThemes = {};
+    this.loadThemes();
+    Theme.getAll().then((registry: API.Registry) => (this.themes = registry));
+  }
+
+  loadThemes() {
     Theme.list().then((themes) => {
       let installed: { [key: string]: boolean } = {};
       themes.forEach((theme) => {
         installed[theme.name] = true;
       });
       this.installedThemes = installed;
+      m.redraw();
     });
-    Theme.getAll().then((registry: API.Registry) => (this.themes = registry));
   }
 
   themeInstalled(name: string): boolean {
     return !!this.installedThemes[name];
   }
 
+  installTheme(p: API.Package) {
+    if (this.installing) {
+      return;
+    }
+    this.installing = p.name;
+    Theme.install(p).then(() => {
+      Toaster.add(this.installing + ' theme installed.');
+      this.installing = null;
+      return this.loadThemes();
+    });
+  }
+
   view() {
-    let themeInstall = (p: API.Package) => {
-      return () => {
-        if (this.installing) {
-          return;
-        }
-        this.installing = p.name;
-        m.redraw();
-        Theme.install(p).then(() => {
-          this.installing = null;
-          m.redraw();
-        });
-      };
-    };
     let packages = this.themes && this.themes.packages ? this.themes.packages : [];
     let themes = packages.map((p: API.Package) => {
-      let themeControl;
       if (this.themeInstalled(p.name)) {
-        themeControl = (
-          <a class='tr' href={`/admin/themes/${p.name}`} oncreate={m.route.link}>
-            installed
-          </a>
+        return (
+          <LinkRow href={`/admin/themes/${p.name}`} link>
+            <div>{p.name}</div>
+            <div>{p.vcsUrl}</div>
+            <div>installed</div>
+          </LinkRow>
         );
-      } else {
-        let themeControlClasses = ['button', 'button--small'];
-        if (!!this.installedThemes) {
-          themeControlClasses.push('button--disabled');
-        } else {
-          themeControlClasses.push('button--blue');
-        }
-        themeControl = (
+      }
+
+      return (
+        <Row center>
+          <div>{p.name}</div>
+          <div>{p.vcsUrl}</div>
           <a
+            class='button button--small button--blue'
             disabled={!!this.installing}
-            class={themeControlClasses.join(' ')}
-            onclick={themeInstall(p)}
+            onclick={() => this.installTheme(p)}
           >
             install
           </a>
-        );
-        return (
-          <div class='tr'>
-            <div>{p.name}</div>
-            <div>{p.vcsUrl}</div>
-            {themeControl}
-          </div>
-        );
-      }
+        </Row>
+      );
     });
 
     return (
       <div>
         <h1>Theme Manager</h1>
-        {!this.installing ? '' : <div>{`Installing theme ${this.installing}...`}</div>}
         <div class='table'>{themes}</div>
+        <ModalComponent
+          title='Installing theme...'
+          visible={() => !!this.installing}
+          toggle={() => {}}
+        >
+          {Loader}
+        </ModalComponent>
       </div>
     );
   }
