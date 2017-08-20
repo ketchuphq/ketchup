133b62ed71f1038efb81d9e8ceb680765c3d224e
tp <tp@octavore.com>
2017-08-19 22:48:56 -0700

    api: Add default placeholders to data list.

--- server/api/data.go
+++ server/api/data.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/proto"
 	"github.com/julienschmidt/httprouter"
 
 	"github.com/ketchuphq/ketchup/db/bolt"
@@ -13,11 +15,52 @@ import (
 	"github.com/ketchuphq/ketchup/util/errors"
 )
 
+var defaultPlaceholders = []*models.Data{
+	{
+		Key: proto.String("title"),
+		Type: &models.Data_Short{
+			Short: &models.ContentString{
+				Type: models.ContentTextType_text.Enum(),
+			},
+		},
+	},
+}
+
+func (m *Module) addAllPlaceholders(data []*models.Data) ([]*models.Data, error) {
+	keys := map[string]*models.Data{}
+	for _, d := range data {
+		keys[d.GetKey()] = d
+	}
+
+	for _, p := range defaultPlaceholders {
+		if _, ok := keys[p.GetKey()]; !ok {
+			data = append(data, proto.Clone(p).(*models.Data))
+		}
+	}
+
+	themes, err := m.Templates.ListThemes()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, theme := range themes {
+		for _, p := range theme.GetPlaceholders() {
+			key := fmt.Sprintf("%s.%s", theme.GetName(), p.GetKey())
+			if _, ok := keys[key]; !ok {
+				keys[key] = proto.Clone(p).(*models.Data)
+				data = append(data, keys[key])
+			}
+		}
+	}
+	return data, nil
+}
+
 func (m *Module) ListData(rw http.ResponseWriter, req *http.Request, par httprouter.Params) error {
 	data, err := m.DB.ListData()
-	if _, ok := err.(bolt.ErrNoKey); ok {
-		return router.ErrNotFound
+	if err != nil {
+		return err
 	}
+	data, err = m.addAllPlaceholders(data)
 	if err != nil {
 		return err
 	}
