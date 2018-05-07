109957df2ae9fb45bf5767047d6ef1eeb0e49785
tp <tp@octavore.com>
2018-05-06 19:57:17 -0700

    admin: Add lib for file API requests.

--- admin/src/js/lib/requests.ts
+++ admin/src/js/lib/requests.ts
@@ -4,9 +4,16 @@ export const get = (url: string) =>
     credentials: 'same-origin',
   });
 
-export const post = (url: string, data?: any) =>
+export const post = (url: string, data?: any, json = true) =>
   fetch(url, {
     method: 'POST',
+    credentials: 'same-origin',
+    body: data && json ? JSON.stringify(data) : data,
+  });
+
+export const del = (url: string, data?: any) =>
+  fetch(url, {
+    method: 'DELETE',
     credentials: 'same-origin',
     body: data ? JSON.stringify(data) : null,
   });
