3fa7681635e3996c8d3cb92affadaac66c5eb3e6
tp <tp@octavore.com>
2018-05-15 18:07:22 -0700

    tls: Move commands into own module.

--- server/tls/module.go
+++ server/tls/module.go
@@ -48,38 +48,7 @@ type Module struct {
 }
 
 func (m *Module) Init(c *service.Config) {
-	c.AddCommand(&service.Command{
-		Keyword:    "tls:provision <example.com> <my@email.com>",
-		ShortUsage: `Provision an ssl cert for the given domain and email`,
-		Usage: `Provision an ssl cert for the given domain.
-Required params: domain to provision a cert for; contact email for Let's Encrypt.`,
-		Flags: []*service.Flag{{Key: "agree"}},
-		Run: func(ctx *service.CommandContext) {
-			ctx.RequireExactlyNArgs(2)
-			if !ctx.Flags["agree"].Present() {
-				fmt.Print("Please provide the --agree flag to indicate that you agree to Let's Encrypt's TOS. \n")
-				return
-			}
-			err := m.ObtainCert(ctx.Args[1], ctx.Args[0])
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Println("success!")
-		},
-	})
-
-	c.AddCommand(&service.Command{
-		Keyword:    "tls:renew",
-		ShortUsage: `Renew SSL certs`,
-		Usage:      `Renew installed SSL certs.`,
-		Run: func(ctx *service.CommandContext) {
-			err := m.renewExpiredCerts()
-			if err != nil {
-				fmt.Println("error:", err)
-			}
-			fmt.Println("success!")
-		},
-	})
+	m.registerCommands(c)
 	c.Setup = func() error {
 		m.renewWithinInterval = defaultRenewWithinInterval
 		m.keystore = &keystore.KeyStore{Dir: m.Config.Config.DataDir}
